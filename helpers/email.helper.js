0de395311dd9e18f7fcc64deccf1f9e40bf33c54
Anh Phat <anhphat@Anphacats-MacBook-Pro.local>
2021-06-08 21:19:39 +0700

    Modify

--- helpers/email.helper.js
+++ helpers/email.helper.js
@@ -19,11 +19,14 @@ emailInternalHelper.createTemplatesIfNotExists = async () => {
         description: "This template is used when user register new email",
         email_key: "verify_email",
         from: "phat.hna2311@gmail.com",
-        subject: "Hi %username%, Welcome to Social Blog",
-        html: `Hi <strong>%username%</strong>, <br /> Thanks your for registration.<br />
-        Please confirm your email address by clicking on the link below. <br />%code%<br />
-        If you have any difficulty during the sign-up, do get in touch with our support team:apply@coderschool.vn
-        <br/>Codershcool Team`,
+        subject: "Hi %username%, Welcome to Rune House",
+        html: `<h3>Hi %username%</h3>,<br /><br />
+        To complete email verification, please press the button below.<br /><br />
+        <a href="%code%" target="_blank" style="16px; font-family: Helvetica, Arial, sans-serif; color: #ffffff; font-weight:normal; text-align:center; background-color: #d67b35; text-decoration: none; border: none; -webkit-border-radius: 100px; -moz-border-radius: 100px; border-radius: 100px; display: inline-block;"><span style="font-size: 16px; font-family: Helvetica, Arial, sans-serif; color: #ffffff; font-weight:normal; line-height:1.5em; text-align:center;">Click Here</span></a>
+        or verify using this link: %code%<br /><br /><br />
+        If you did not create an account using this address, please ignore this mail.<br /><br /><br />
+        If you have any difficulty during the sign-up, do get in touch with our support hnanhphat@gmail.com
+        <br/>Barry Hoang`,
         variables: ["username", "code"],
       });
       await emailTemplate.save();
